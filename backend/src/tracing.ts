baeeb5bce0e853e0e57d8064cef536aef14fea03
Akshay Patel <Akshaysukhadiya41@gmail.com>
2025-06-16 10:04:48 -0400

    Tracing Update

--- backend/src/tracing.ts
+++ backend/src/tracing.ts
@@ -1,28 +1,38 @@
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { resourceFromAttributes } from '@opentelemetry/resources';
+import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-base';
+import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from '@opentelemetry/semantic-conventions';
 import logger from './logger';
 
 const resource = resourceFromAttributes({
-  'service.name': 'adobe-task-backend-api',
-  'service.version': '1.0.0',
+  [ATTR_SERVICE_NAME]: 'adobe-task-backend-api',
+  [ATTR_SERVICE_VERSION]: '1.0.0',
 });
 
-const traceExporter = new OTLPTraceExporter({
-  url: 'http://localhost:4318/v1/traces',
-});
+// You can replace ConsoleSpanExporter with any other exporter as needed
+// For example, you can use Jaeger, Zipkin, or any other exporter supported by OpenTelemetry
+const traceExporter = new ConsoleSpanExporter();
 
+// Initialize the OpenTelemetry SDK with the resource and exporters
 const sdk = new NodeSDK({
   resource: resource,
   traceExporter: traceExporter,
   instrumentations: [getNodeAutoInstrumentations()]
 });
 
-
+// Start the SDK to begin tracing
 try {
   sdk.start();
   logger.info('Tracing initialized');
 } catch (err) {
   logger.error('Error initializing tracing', err);
 }
+
+// gracefully shut down the SDK on process exit
+process.on('SIGTERM', () => {
+  sdk.shutdown()
+    .then(() => logger.info('Tracing terminated'))
+    .catch((error) => logger.error('Error terminating tracing', error))
+    .finally(() => process.exit(0));
+});
